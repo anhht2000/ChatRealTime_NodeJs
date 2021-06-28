b082f3c4d39b9c375c4684c03f6b77c1f0a3d62d
tuananhcx2000 <tuananhcx2000@gmail.com>
2021-06-28 10:17:26 +0700

    ok

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'resource','views'));
 
 //nhung file tinh
-app.use(express.static(path.join(__dirname, '\\public')));
+app.use(express.static(path.join(__dirname, 'public')));
 //socket
 let arrayUser = [];
 let arrUserid = [];
